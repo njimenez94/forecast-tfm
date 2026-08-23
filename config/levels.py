0f8a4dbdc40e023f99a4f9c4d441ebbfb36d7f05
njimenez94 <jd.nicolas1@gmail.com>
2026-08-23 17:11:14 -0400

    Fix formatting issue in LEVELS definition for item level

--- config/levels.py
+++ config/levels.py
@@ -29,7 +29,7 @@ LEVELS = [
     Level(7,  "state_dept", ("dept_id", "cat_id", "state_id"),                              ("daily",)),
     Level(8,  "store_cat",  ("cat_id", "store_id", "state_id"),                             ("daily",)),
     Level(9,  "store_dept", ("dept_id", "cat_id", "store_id", "state_id"),                  ("daily",)),
-    Level(10, "item",       ("dept_id", "cat_id", "item_id"),                               ("weekly", "daily"),              ("dept_id")),
+    Level(10, "item",       ("dept_id", "cat_id", "item_id"),                               ("weekly", "daily"),              ("dept_id",)),
     Level(11, "item_state", ("dept_id", "cat_id", "item_id", "state_id"),                   ("weekly", "daily"),              ("state_id", "dept_id")),
     Level(12, "item_store", ("dept_id", "cat_id", "item_id", "store_id", "state_id"),       ("weekly", "daily"),              ("store_id", "dept_id")),
 ]
